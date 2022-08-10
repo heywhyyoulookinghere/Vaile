44b38d4b6e2bfb670486b32e6ee910e6e4bdf54c
idk <74161148+heywhyyoulookinghere@users.noreply.github.com>
2022-08-10 07:50:33 +0100

    Create Vaile.lua

---
+++ Vaile.lua
@@ -0,0 +1,91 @@
+local prefix = "#"
+
+function sendnotif(msg)
+  game.StarterGUI:SetCore("SendNotification", {
+    Title = "Vaile";
+    Text = msg;
+    Duration = 1;
+    })
+end
+
+local StagePosition = -76.806, 57.6, -214.826
+local DJPosition = -90.536, 59.07, -215.866
+
+local Player = game.Players.LocalPlayer
+local Character = Player.Character
+
+Player.Chatted:Connect(function(msg)
+    local split = string.split(msg," ")
+    local lower = string.lower(split[1])
+    
+    if lower == prefix.."prefix" then
+      prefix = split[2]
+      task.wait(0.1)
+      sendnotif("Prefix has been changed to "..split[2]..".")
+   end
+end)
+
+Player.Chatted:Connect(function(msg)
+    local split = string.split(msg," ")
+    local lower = string.lower(split[1])
+    
+    if lower == prefix.."stage" then
+      local Root = Character:FindFirstChild("HumanoidRootPart")
+      Root.CFrame = CFrame.new(Vector3.new(StagePosition))
+      task.wait(0.1)
+      sendnotif("Moved you to the stage.")
+   end
+end)
+
+Player.Chatted:Connect(function(msg)
+    local split = string.split(msg," ")
+    local lower = string.lower(split[1])
+    
+    if lower == prefix.."dj" then
+      local Root = Character:FindFirstChild("HumanoidRootPart")
+      Root.CFrame = CFrame.new(Vector3.new(DJPosition))
+      task.wait(0.1)
+      sendnotif("Moved you to the DJ.")
+   end
+end)
+
+
+Player.Chatted:Connect(function(msg)
+    local split = string.split(msg," ")
+    local lower = string.lower(split[1])
+    
+    if lower == prefix.."spamvote" then
+    if game.Workspace.RapBattles.Rappers.player1.Value or game.Workspace.RapBattles.Rappers.player2.Value == split[2] then
+      if game.Workspace.RapBattles.Rappers.player1.Value == split[2] then
+         for i = 0,1 do 
+             game.Workspace.Votes:FireServer(false,"p1")
+             game:GetService("RunService").Heartbeat:Wait()
+         end
+      end
+      if game.Workspace.RapBattles.Rappers.player2.Value == split[2] then
+          for i = 0,1 do
+            game.Workspace.Votes:FireServer(false,"p2")
+            game:GetService("RunService").Heartbeat:Wait()
+         end
+      end
+      task.wait(0.1)
+      sendnotif("Successfully spammed the votes of "..split[2]..".")
+   else
+      sendnotif(split[2].." isn't on stage.")
+   end
+end)
+  
+Player.Chatted:Connect(function(msg)
+   local lower = string.lower(msg)
+      
+   if lower == prefix.."cmds" then
+      print("Vaile")
+      print(".....")
+      print("prefix -- changes the prefix.")
+      print("dj -- teleports you to booth.")
+      print("stage -- teleports you to the stage.")
+      print("spamvote -- spams the vote of someone on stage. (use when the voting menu comes up)")
+      task.wait(0.1)
+      sendnotif("Please check the console for commands with /console.")
+   end
+end)
